f391d4ab80cd09a583c06df7de2acfed1339b94e
Mikhail <rscience.code@gmail.com>
2017-12-17 15:43:08 +0300

    refactor create command

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,54 +1,24 @@
 const fs = require('fs');
 const path = require('path');
-const helpers = require('../helpers');
 const config = require('../../config');
+const getRootPath = require('../helpers/getRootPath');
+const createFolderPath = require('../helpers/createFolderPath');
+const createFile = require('../helpers/createFile');
 
 module.exports = (entity, name, options) => {
-  console.log(options);
 // loading settings
-  const rc = helpers.fileExists(config.rcName) 
-    ? helpers.readRC(config.rcName)
-    : null;
+  const rootPath = getRootPath(entity);
+  const folderPath = createFolderPath(rootPath, name);
 
-  if (!rc) {
-    console.log(chalk.red('Error! rc file not found!'));
-    process.exit();
-  }
+  const fileName = name.split('/').pop();
 
-  const createPath = rc[entity];
+  createFile(rootPath, folderPath, fileName);
 
-  if (!createPath) {
-    console.log(`no creation path for ${entity}. check your settings`);
-    process.exit();
-  }
-
-  if (!helpers.directoryExists(createPath)) {
-    const entityPath = path.resolve(process.cwd(), createPath)
-    fs.mkdirSync(entityPath);
-  }
-  
-  const folder = path.resolve(process.cwd(), createPath, `${name}`);
-  fs.mkdirSync(folder);
-
-  const fullPath = path.resolve(folder, `${name}.js`);
-  const snippetPath = path.resolve(process.cwd(), `./.snippets/${entity}.js`);
-
-  const snippet = fs.readFileSync(snippetPath, { encoding: 'utf8' });
-
-  const file = snippet.replace(/%name%/g, name);
-
-  const index = path.resolve(folder, `index.js`);
-
-  fs.writeFile(fullPath, file, () => console.log(`${name}.js created!`));
+  const index = path.resolve(rootPath, folderPath, `index.js`);
 
   if (options.c) {
-    const fullPath = path.resolve(folder, `${name}Container.js`);
-    const snippetPath = path.resolve(process.cwd(), `./.snippets/container.js`);
-    const snippet = fs.readFileSync(snippetPath, { encoding: 'utf8' });
-    const file = snippet.replace(/%name%/g, name);
-    fs.writeFile(fullPath, file, () => console.log(`${name}Container.js created!`));
-
-    const exportLine = `export { default as ${name} } from './${name}Container';\n`
+    createFile(rootPath, folderPath, `${fileName}Container`);
+    const exportLine = `export { default as ${fileName} } from './${fileName}Container';\n`
     fs.writeFile(index, exportLine, () => console.log('index.js created!'));
   } else {
     const exportLine = `export { default as ${name} } from './${name}';\n`
